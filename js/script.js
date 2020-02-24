d5aff6e6a35590c67caab5c1c801aa2943eae6af
Sander Byrkjeland <sunbyr@gmail.com>
2020-02-24 23:58:42 +0100

    Working on it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+const BASE_URL = "http://api.tvmaze.com/shows";
+let cachedMovieArray = [];
+let contentContainer = document.getElementById("contentContainer");
 
+fetch(BASE_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    loadShows(json);
+  })
+  .catch(function(error) {
+    console.dir(error);
+  });
+
+function loadShows(json) {
+  const shows = json;
+  console.dir(json);
+
+  const container = document.querySelector(".box");
+  let newHTML = "";
+
+  for (let i = 0; i < shows.length; i++) {
+    const showImage = shows[i].image.medium;
+
+    const details = `<img class="image" src=${showImage} alt="Show name">`;
+
+    newHTML += details;
+  }
+  container.innerHTML = newHTML;
+}
